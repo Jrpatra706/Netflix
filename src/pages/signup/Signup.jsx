9c65007ad06f9f49d95a54fd0766f3d0e62009e7
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-12-07 00:11:38 +0530

    added netflix background to signup section

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      signup
-    </div>
+    <>
+      <div className=' w-full h-screen'>
+        <img  className='  sm:block absolute w-full h-full object-cover'
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_small.jpg"  alt="///"
+        />
+        <div className=' bg-black/70 fixed top-0 left-0 w-full h-screen'/>
+      </div>
+    </>
   )
 }
 
